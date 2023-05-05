581856da5f12c4771ee4aed7424c6718a1db3c41
victor-brillant <victor.brillant.dev23@gmail.com>
2023-05-05 17:45:13 +0300

    pagination

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
+"use client"; // this is a client component
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { useState } from 'react';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,9 +15,65 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  const [showModal, setShowModal] = useState(false);
   return (
     <html lang="en">
       <body className={inter.className}>
+      {showModal ? (
+        <>
+          <div
+            className="fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto outline-none focus:outline-none"
+          >
+            <div className="relative w-auto max-w-3xl mx-auto my-6">
+              {/*content*/}
+              <div className="relative flex flex-col w-full bg-white border-0 rounded-lg shadow-lg outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid rounded-t border-slate-200">
+                  <h3 className="text-3xl font-semibold">
+                    Sign in
+                  </h3>
+                  <button
+                    className="float-right p-1 ml-auto text-3xl font-semibold leading-none text-black bg-transparent border-0 outline-none opacity-5 focus:outline-none"
+                    onClick={() => setShowModal(false)}
+                  >
+                    <span className="block w-6 h-6 text-2xl text-black bg-transparent outline-none opacity-5 focus:outline-none">
+                      ×
+                    </span>
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative flex-auto p-6">
+                  <p className="my-4 text-lg leading-relaxed text-slate-500">
+                    I always felt like I could do anything. That’s the main
+                    thing people are controlled by! Thoughts- their perception
+                    of themselves! They're slowed down by their perception of
+                    themselves. If you're taught you can’t do anything, you
+                    won’t do anything. I was taught I could do everything.
+                  </p>
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-end p-6 border-t border-solid rounded-b border-slate-200">
+                  <button
+                    className="px-6 py-2 mb-1 mr-1 text-sm font-bold text-red-500 uppercase transition-all duration-150 ease-linear outline-none background-transparent focus:outline-none"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Close
+                  </button>
+                  <button
+                    className="px-6 py-3 mb-1 mr-1 text-sm font-bold text-white uppercase transition-all duration-150 ease-linear rounded shadow outline-none bg-emerald-500 active:bg-emerald-600 hover:shadow-lg focus:outline-none"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Save Changes
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="fixed inset-0 z-40 bg-black opacity-25"></div>
+        </>
+      ) : null}
         <header className="fixed w-full">
             <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
                 <div className="flex flex-wrap items-center justify-between max-w-screen-xl mx-auto">
@@ -24,7 +82,7 @@ export default function RootLayout({
                         <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">OpenSea</span>
                     </a>
                     <div className="flex items-center lg:order-2">
-                        <a href="#" className="text-primary-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Sign in</a>
+                        <a href="#" onClick={() => setShowModal(true)}  className="text-primary-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Sign in</a>
                         <a href="#" className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">Sign up</a>
                         <button data-collapse-toggle="mobile-menu-2" type="button" className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
                             <span className="sr-only">Open main menu</span>
@@ -35,10 +93,19 @@ export default function RootLayout({
                     <div className="items-center justify-between hidden w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
                         <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                             <li>
-                                <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Drop</a>
+                                <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Menu1</a>
                             </li>
                             <li>
-                                <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">State</a>
+                                <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Menu2</a>
+                            </li>
+                            <li>
+                                <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Menu3</a>
+                            </li>
+                            <li>
+                                <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Menu4</a>
+                            </li>
+                            <li>
+                                <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Menu5</a>
                             </li>
                         </ul>
                     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,50 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   const [nfts, setNfts] = useState([]);
+  const [search, setSearch] = useState("");
+  const [page, setPage] = useState(1);
+  const options = {method: 'GET', headers: {accept: 'application/json'}};
+  
   useEffect(() => {
-    fetch('https://eth-mainnet.g.alchemy.com/v2/XyiOijMVoy42To3frN6_SDrXPzcQ0EaC/getNFTs/?owner=vitalik.eth&offset=0&limit=10')
+    fetch('https://testnets-api.opensea.io/api/v1/assets?order_direction=desc&offset='+page+'&limit=50', options)
       .then(response => response.json())
-      .then(data => setNfts(data.ownedNfts)
+      .then(data => setNfts(data.assets)
       )
       .catch(error => console.error(error));
   }, []);
+
+  function nextPage() {
+    setPage(page+1)
+    watchSearch('');
+  }
+  function prevPage() {
+    if(page>0){
+      setPage(page-1)
+      watchSearch('');
+    }
+  }
+
+  function watchSearch(value){
+    fetch('https://testnets-api.opensea.io/api/v1/assets?order_direction=desc&offset='+page+'&limit=50&collection='+value, options)
+      .then(response => response.json())
+      .then(data => 
+        {
+          if (data.assets && data.assets.length>0) {
+            console.log('====================================');
+            console.log('if');
+            console.log('====================================');
+            setNfts(data.assets)
+          } else {
+            console.log('====================================');
+            console.log('else');
+            console.log('====================================');
+            const vide = [];
+            setNfts(vide)
+          }
+        }
+      )
+      .catch(error => console.error(error));
+  };
   return (
     <main className="flex-col items-center justify-between min-h-screen pt-24 bg-white">
       <section className="bg-white dark:bg-gray-900">
@@ -23,7 +60,7 @@ export default function Home() {
                 <p className="font-light text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-400">A collection of crazy typewriters from the mind of a mad mechanic. With tributes to the great inventors who perfected them and the artists who used them to change the world. These machines give their owners the right to pilot the largest free magazine dedicated to the web3.</p>
                 <div className="flex grid justify-center grid-cols-2">
                 <div className='flex justify-center rounded-lg shadow search-block'>
-                    <input type="text" placeholder='Search' className='w-full px-3 py-2 rounded-md rounded-r-none outline-none bg-gray-50 dark:bg-gray-800 dark:border-gray-700' />
+                    <input type="text" placeholder='Search' className='w-full px-3 py-2 rounded-md rounded-r-none outline-none bg-gray-50 dark:bg-gray-800 dark:border-gray-700' onChange={(e) => watchSearch(e.target.value)} />
                     <a href="#" className="text-white text-center bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg rounded-l-none text-sm px-4 lg:px-5 py-2 lg:py-2.5 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800 block ">Search
                     </a>
                   </div>
@@ -74,14 +111,14 @@ export default function Home() {
                     <div key={item} className="items-center rounded-lg shadow item-nft item-nps bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
                       <div className='w-full'>
                         <a className='lien-img' href="#">
-                            <img  className="w-full rounded-lg rounded-b-none round" src={item.contractMetadata?.openSea?.imageUrl} alt={item.contractMetadata?.name}/>
+                            <img  className="w-full rounded-lg rounded-b-none round" src={item.collection.image_url?item.collection.image_url:"https://i.seadn.io/gae/t-bM7LBgt74MRll4skaW3DBwmLK1Z-okq5kipkeTCa1v5lBErDsORn86nHhfZPdC3eTR6uqYQ1IEah3QfsKSnuzlu5y-8dKLzug_?auto=format&w=512"} alt={item.collection?.name}/>
                         </a>
                         <div className="p-5 block-details">
-                            <p className="font-bold tracking-tight text-gray-900 dark:text-white limite2-lign">
-                            {item.contractMetadata?.name} #{item?.balance}
+                            <p className="font-bold tracking-tight text-gray-900 dark:text-white limite1-lign">
+                            {item.collection?.name}
                             </p>
-                            <p className="mt-3 font-bold tracking-tight text-gray-900 dark:text-white limite3-lign">
-                            {item.contractMetadata?.openSea?.description}
+                            <p className="mt-3 font-bold tracking-tight text-gray-900 dark:text-white limite2-lign">
+                            {item.collection?.description}
                             </p>
                         </div>
                       </div>
@@ -95,7 +132,10 @@ export default function Home() {
                     </div>  
                   )
                 )
-              }</>
+              }
+              {/* <button onClick={prevPage()}>Preview</button>
+              <button onClick={nextPage()}>Next</button> */}
+              </>
             }
             </div>  
         </div>
